373df9ed43a1fdea5e3ec2ee1ed4016d7a74a920
leifeggenfellner <eggenfellner@protonmail.com>
2022-09-14 09:29:52 +0200

    create template files

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const nameContainer = document.getElementById('name-container');
+const changeNameButton = document.getElementById('change-name')
+
+const names = ['Edvard', 'Leif', 'Christoffer', 'Scott', 'Simon', 'Sepanta'];
+
+const generateName = (names) => {
+    const index = Math.floor(Math.random() * names.length);
+    return names[index];
+}
+const displayName = (names) => {
+    let name = generateName(names);
+    nameContainer.innerHTML = name
+}
+changeNameButton.onclick = () => {
+    displayName(names);
+}
