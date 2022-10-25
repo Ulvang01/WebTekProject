120b65aa7fdb48df16dbf8005b321436c509c1ba
Lint Action <lint-action@samuelmeuli.com>
2022-10-25 12:09:54 +0000

    Fix code style issues with Prettier

--- src/js/colorgenerator.js
+++ src/js/colorgenerator.js
@@ -1,54 +1,55 @@
-const hexColor = "#00ff00";
+const hexColor = '#00ff00';
 
 function hexToRGB(hex) {
-    //
-    let r = 0, g = 0, b = 0;
-    if (hex.length == 4) {
-        r = "0x" + hex[1] + hex[1];
-        g = "0x" + hex[2] + hex[2];
-        b = "0x" + hex[3] + hex[3];
-    } else if (hex.length == 7) {
-        r = "0x" + hex[1] + hex[2];
-        g = "0x" + hex[3] + hex[4];
-        b = "0x" + hex[5] + hex[6];
-    }
+	//
+	let r = 0,
+		g = 0,
+		b = 0;
+	if (hex.length == 4) {
+		r = '0x' + hex[1] + hex[1];
+		g = '0x' + hex[2] + hex[2];
+		b = '0x' + hex[3] + hex[3];
+	} else if (hex.length == 7) {
+		r = '0x' + hex[1] + hex[2];
+		g = '0x' + hex[3] + hex[4];
+		b = '0x' + hex[5] + hex[6];
+	}
 
-    return [parseInt(r), parseInt(g), parseInt(b)];
+	return [parseInt(r), parseInt(g), parseInt(b)];
 }
 
 function hexToHSL(hex) {
-    let rgb = hexToRGB(hex);
-    let r = rgb[0];
-    let g = rgb[1];
-    let b = rgb[2];
-    // converting from rgb to HSL
-    r /= 255;
-    g /= 255;
-    b /= 255;
-    let cmin = Math.min(r,g,b),
-    cmax = Math.max(r,g,b),
-    delta = cmax - cmin,
-    h = 0,
-    s = 0,
-    l = 0;
+	let rgb = hexToRGB(hex);
+	let r = rgb[0];
+	let g = rgb[1];
+	let b = rgb[2];
+	// converting from rgb to HSL
+	r /= 255;
+	g /= 255;
+	b /= 255;
+	let cmin = Math.min(r, g, b),
+		cmax = Math.max(r, g, b),
+		delta = cmax - cmin,
+		h = 0,
+		s = 0,
+		l = 0;
 
-    if (delta == 0) h = 0;
-    else if (cmax == r) h = ((g - b) / delta) % 6;
-    else if (cmax == g) h = (b - r) / delta + 2;
-    else h = (r - g) / delta + 4;
+	if (delta == 0) h = 0;
+	else if (cmax == r) h = ((g - b) / delta) % 6;
+	else if (cmax == g) h = (b - r) / delta + 2;
+	else h = (r - g) / delta + 4;
 
-    h = Math.round(h * 60);
+	h = Math.round(h * 60);
 
-    if (h < 0) h += 360;
+	if (h < 0) h += 360;
 
-    l = (cmax + cmin) / 2;
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
+	l = (cmax + cmin) / 2;
+	s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+	s = +(s * 100).toFixed(1);
+	l = +(l * 100).toFixed(1);
 
-    return [h, s, l];
+	return [h, s, l];
 }
 
 console.log(hexToRGB(hexColor));
 console.log(hexToHSL(hexColor));
-
