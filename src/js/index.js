178806ec9f1e12e13355c0f8ad51dde650d63e35
leifeggenfellner <eggenfellner@protonmail.com>
2022-11-03 15:01:47 +0100

    feat(animation): add page load animation
    added a load animation for the page content

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,3 @@
-const colorInput = document.getElementById('color-input');
+import changeColor from './colorInputHandler.js';
 
-const changeColor = () => {
-	const color = colorInput.value;
-	const reg = /^#([0-9a-f]{3}){1,2}$/i;
-	if (reg.test(color)) {
-		colorInput.style.backgroundColor = color;
-	}
-
-	const bgColor = window
-		.getComputedStyle(colorInput, null)
-		.getPropertyValue('background-color')
-		.split('(')[1]
-		.split(')')[0]
-		.split(',');
-	const colorYIQ = Math.round(
-		(bgColor[0] * 299 + bgColor[1] * 587 + bgColor[2] * 114) / 1000
-	);
-
-	const textColor = colorYIQ >= 100 ? '#2e3440' : '#f9f7f7';
-	colorInput.style.color = textColor;
-};
-
-colorInput.addEventListener('input', changeColor);
+document.getElementById('color-input').oninput = () => changeColor();
