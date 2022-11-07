dbbd5bbaa7549b1cd76ead1c53788a3c2e34a43c
leifeggenfellner <eggenfellner@protonmail.com>
2022-11-07 16:03:33 +0100

    feat(about): finish about page

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,25 @@ import createComponents from './colorElementCreator.js';
 document.getElementById('color-input').oninput = () => changeColor();
 
 const colorPalletes = {
-    "palette1": ["#F9F7F7", "#FD7702", "#65C6C4", "#408AB4", "#34699A", "#112D4E"],
-    "palette2": ["#F9F871", "#FFC75F", "#FF9671", "#FF6F91", "#D65DB1", "#845EC2"]
+	'Palette 1': [
+		'#F9F7F7',
+		'#FD7702',
+		'#65C6C4',
+		'#408AB4',
+		'#34699A',
+		'#112D4E'
+	],
+	'Palette 2': [
+		'#F9F871',
+		'#FFC75F',
+		'#FF9671',
+		'#FF6F91',
+		'#D65DB1',
+		'#845EC2'
+	]
 };
 
 document.getElementById('submit-btn').onclick = () => {
-    createComponents(colorPalletes);
-    document.getElementById('color-section').scrollIntoView();
-}
+	createComponents(colorPalletes);
+	document.getElementById('color-section').scrollIntoView();
+};
