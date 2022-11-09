5a8dc3b1448afacda425986f87086ec13e07f35f
leifeggenfellner <eggenfellner@protonmail.com>
2022-11-09 11:16:51 +0100

    feat(PaletteGenerator): finish generation of colors

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,17 @@
 import changeColor from './colorInputHandler.js';
 import createComponents from './colorElementCreator.js';
+import PaletteGenerator from './paletteGenerator.js';
 
 document.getElementById('color-input').oninput = () => changeColor();
 
-const colorPalletes = {
-	'Palette 1': [
-		'#F9F7F7',
-		'#FD7702',
-		'#65C6C4',
-		'#408AB4',
-		'#34699A',
-		'#112D4E'
-	],
-	'Palette 2': [
-		'#F9F871',
-		'#FFC75F',
-		'#FF9671',
-		'#FF6F91',
-		'#D65DB1',
-		'#845EC2'
-	]
-};
-
 document.getElementById('submit-btn').onclick = () => {
-	createComponents(colorPalletes);
+	const colorGenerator = new PaletteGenerator(
+		document.getElementById('color-input').value
+	);
+
+	colorGenerator.generatePalettes();
+
+	createComponents(colorGenerator.palettes);
 	document.getElementById('color-section').scrollIntoView();
 
 	document.body.insertAdjacentHTML(
