01de44008ee62fd026dc167818e312f6602d2952
leifeggenfellner <eggenfellner@protonmail.com>
2022-11-09 15:10:17 +0100

    feat(onclick): lint hexcode input

--- src/js/index.js
+++ src/js/index.js
@@ -5,36 +5,39 @@ import PaletteGenerator from './paletteGenerator.js';
 document.getElementById('color-input').oninput = () => changeColor();
 
 document.getElementById('submit-btn').onclick = () => {
-	const colorGenerator = new PaletteGenerator(
-		document.getElementById('color-input').value
-	);
+	const color = document.getElementById('color-input').value;
 
-	colorGenerator.generatePalettes();
+	if (color.includes('#') && (color.length === 4 || color.length === 7)) {
+		const colorGenerator = new PaletteGenerator(color);
+		colorGenerator.generatePalettes();
 
-	createComponents(colorGenerator.palettes);
-	document.getElementById('color-section').scrollIntoView();
+		createComponents(colorGenerator.palettes);
+		document.getElementById('color-section').scrollIntoView();
 
-	if (!document.querySelector('footer')) {
-		document.body.insertAdjacentHTML(
-			'beforeend',
-			`
-			<footer>
-				<div class="wrapper">
-					<div class="logo">
-						<img src="./img/anchor.svg" alt="anchor" class="anchor-footer" />
-						<p>Color<span class="logo-text">Ocean</span></p>
+		if (!document.querySelector('footer')) {
+			document.body.insertAdjacentHTML(
+				'beforeend',
+				`
+				<footer>
+					<div class="wrapper">
+						<div class="logo">
+							<img src="./img/anchor.svg" alt="anchor" class="anchor-footer" />
+							<p>Color<span class="logo-text">Ocean</span></p>
+						</div>
+						<a href="#home" id="back-to-top">Back to the top</a>
 					</div>
-					<a href="#home" id="back-to-top">Back to the top</a>
-				</div>
-			</footer>`
-		);
-	}
-
-	window.onscroll = () => {
-		if (window.scrollY <= 0) {
-			document.querySelector('footer').style.display = 'none';
-		} else {
-			document.querySelector('footer').style.display = 'flex';
+				</footer>`
+			);
 		}
-	};
+
+		window.onscroll = () => {
+			if (window.scrollY <= 0) {
+				document.querySelector('footer').style.display = 'none';
+			} else {
+				document.querySelector('footer').style.display = 'flex';
+			}
+		};
+	} else {
+		console.error('Please enter a valid color code');
+	}
 };
