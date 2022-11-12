ed60ed012d97eb2c474818f8e13706aa18cfd245
Lint Action <lint-action@samuelmeuli.com>
2022-11-12 16:25:23 +0000

    Fix code style issues with Prettier

--- src/js/mailHandler.js
+++ src/js/mailHandler.js
@@ -1,11 +1,11 @@
 document.getElementById('submit').onclick = (event) => {
-    event.preventDefault();
-    const subject = document.getElementById('subject').value;
+	event.preventDefault();
+	const subject = document.getElementById('subject').value;
 
-    const body = document.getElementById('problem').value;
+	const body = document.getElementById('problem').value;
 
-    window.open(
-        `mailto:test@example.com?subject=${subject}&body=${body}`,
-        '_self'
-    );
+	window.open(
+		`mailto:test@example.com?subject=${subject}&body=${body}`,
+		'_self'
+	);
 };
--- src/js/navGenerator.js
+++ src/js/navGenerator.js
@@ -1,7 +1,7 @@
 window.onload = () => {
-    document.body.insertAdjacentHTML(
-        'afterbegin',
-        `<header>
+	document.body.insertAdjacentHTML(
+		'afterbegin',
+		`<header>
             <nav>
                 <a class="logo" href="index.html">
                     <img src="./img/anchor.svg" alt="anchor" class="anchor" />
@@ -19,41 +19,41 @@ window.onload = () => {
                 </div>
             </nav>
         </header>`
-    );
+	);
 
-    const fileName = location.href.split('/').slice(-1)[0].split('.')[0];
-    fileName == ''
-        ? document.getElementById(`index-nav`).classList.add('inuse')
-        : document.getElementById(`${fileName}-nav`).classList.add('inuse');
+	const fileName = location.href.split('/').slice(-1)[0].split('.')[0];
+	fileName == ''
+		? document.getElementById(`index-nav`).classList.add('inuse')
+		: document.getElementById(`${fileName}-nav`).classList.add('inuse');
 
-    const menuBtn = document.querySelector('.menu-btn');
-    const menuNav = document.querySelector('.nav-list');
-    const navItems = document.querySelectorAll('.nav-item');
+	const menuBtn = document.querySelector('.menu-btn');
+	const menuNav = document.querySelector('.nav-list');
+	const navItems = document.querySelectorAll('.nav-item');
 
-    let showMenu = false;
+	let showMenu = false;
 
-    menuBtn.onclick = () => toggleMenu();
+	menuBtn.onclick = () => toggleMenu();
 
-    const toggleMenu = () => {
-        if (!showMenu) {
-            menuBtn.classList.add('close');
-            menuNav.classList.add('show', 'fly-in');
-            navItems.forEach((item) => item.classList.add('show'));
+	const toggleMenu = () => {
+		if (!showMenu) {
+			menuBtn.classList.add('close');
+			menuNav.classList.add('show', 'fly-in');
+			navItems.forEach((item) => item.classList.add('show'));
 
-            showMenu = true;
-        } else {
-            menuNav.classList.remove('fly-in');
-            menuBtn.classList.remove('close');
-            menuNav.classList.add('fly-out');
-            menuNav.addEventListener(
-                'animationend',
-                () => {
-                    menuNav.classList.remove('show', 'fly-out');
-                    navItems.forEach((item) => item.classList.remove('show'));
-                    showMenu = false;
-                },
-                { once: true }
-            );
-        }
-    };
+			showMenu = true;
+		} else {
+			menuNav.classList.remove('fly-in');
+			menuBtn.classList.remove('close');
+			menuNav.classList.add('fly-out');
+			menuNav.addEventListener(
+				'animationend',
+				() => {
+					menuNav.classList.remove('show', 'fly-out');
+					navItems.forEach((item) => item.classList.remove('show'));
+					showMenu = false;
+				},
+				{ once: true }
+			);
+		}
+	};
 };
